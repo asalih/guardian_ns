d114959a529280f064511dc0432d06002590be92
Ahmet Salih <ahmet.salih@windowslive.com>
2019-10-12 13:29:32 +0300

    logs removed

--- dns_handler.go
+++ dns_handler.go
@@ -29,24 +29,20 @@ func NewDNSHandler() *DNSHandler {
 
 //ServeDNS ...
 func (h *DNSHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
-
-	fmt.Println("Request Incoming:")
 	msg := dns.Msg{}
 	msg.SetReply(r)
 	switch r.Question[0].Qtype {
 	case dns.TypeA:
 		msg.Authoritative = true
 		domain := msg.Question[0].Name
-		fmt.Println("Incoming domain" + domain)
 		address, ok := h.Targets[domain]
+
 		if ok {
 			msg.Answer = append(msg.Answer, &dns.A{
 				Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
 				A:   net.ParseIP(address),
 			})
 		}
-
-		fmt.Println(h.Targets)
 	}
 	w.WriteMsg(&msg)
 }
@@ -55,7 +51,6 @@ func (h *DNSHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 func (h *DNSHandler) LoadTargets() {
 	h.mutex.Lock()
 	defer h.mutex.Unlock()
-	fmt.Printf("Targets loading")
 
 	h.Targets = h.DBHelper.GetTargetsList()
 }
